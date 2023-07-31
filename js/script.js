323ae945caeae0a8666c685dc6ccf4f262cfbe68
Mohammad Javad Rezaei <mjr0924@gmail.com>
2023-08-01 02:27:45 +0330

    add breakpoints and keyframes

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
 //toggle icon navbar
+let menuIcon = document.querySelector("#menu-icon");
+let navbar = document.querySelector(".navbar");
+
+menuIcon.onclick = () => {
+  menuIcon.classList.toggle("bx-x");
+  navbar.classList.toggle("active");
+};
 
 // scroll section
 let sections = document.querySelectorAll("section");
@@ -25,4 +32,8 @@ window.onscroll = () => {
   // sticky header
   let header = document.querySelector("header");
   header.classList.toggle("sticky", window.scrollY > 100);
+
+  //remove toggle icon and navbar when click navbar links (scroll)
+  menuIcon.classList.remove("bx-x");
+  navbar.classList.remove("active");
 };
