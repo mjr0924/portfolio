aefc70008a86ce58e5945a6f97c5ac0965e4b234
Mohammad Javad Rezaei <mjr0924@gmail.com>
2023-08-04 02:53:07 +0330

    added animation to all section

--- js/script.js
+++ js/script.js
@@ -26,6 +26,12 @@ window.onscroll = () => {
           .querySelector("header nav a[href*=" + id + "]")
           .classList.add("active");
       });
+      // active section for animation on scroll
+      sec.classList.add("show-animate");
+    }
+    // if want to use animation that repeats on scroll use this
+    else {
+      sec.classList.remove("show-animate");
     }
   });
 
@@ -36,4 +42,8 @@ window.onscroll = () => {
   //remove toggle icon and navbar when click navbar links (scroll)
   menuIcon.classList.remove("bx-x");
   navbar.classList.remove("active");
+
+  // animation footer on scroll
+  let footer = document.querySelector("footer");
+  footer.classList.toggle('show-animate',this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 };
